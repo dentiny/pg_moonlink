8d1f188a2152e5acdfc399a072490b435e5fed25
dentiny <dentinyhao@gmail.com>
2025-04-29 17:37:28 +0000

    create table and table exists

--- src/moonlink/src/storage/iceberg/s3_catalog.rs
+++ src/moonlink/src/storage/iceberg/s3_catalog.rs
@@ -3,10 +3,11 @@ use std::collections::HashMap;
 ///
 /// TODO(hjiang):
 /// 1. Better error handling.
-/// 2. Implement `list_namespace` function, for now it's not required in snapshot <-> iceberg interaction.
-/// 3. Implement property related functionalities.
-/// 4. The initial version access everything via filesystem, for performance consideration we should cache metadata in memory.
-/// 5. (not related to functionality) Set snapshot retention policy at metadata.
+/// 2. Implement property related functionalities.
+/// 3. (low priority) Implement `list_namespace` function, for now it's not required in snapshot <-> iceberg interaction.
+/// 4. (low priority) Implement `rename_table` function, for now it's not required in snapshot <-> iceberg interaction.
+/// 5. The initial version access everything via filesystem, for performance consideration we should cache metadata in memory.
+/// 6. (not related to functionality) Set snapshot retention policy at metadata.
 ///
 /// Iceberg table format from object storage's perspective:
 /// - namespace_indicator.txt
@@ -29,6 +30,7 @@ use std::collections::HashMap;
 ///
 /// For S3 catalog, all files are stored under the warehouse location, in detail: <warehouse-location>/<namespace>/<table-name>/.
 use std::error::Error;
+use std::fmt::format;
 use std::path::PathBuf;
 use std::vec;
 
@@ -37,7 +39,7 @@ use aws_sdk_s3::config::{Credentials, Region};
 use aws_sdk_s3::primitives::ByteStream;
 use aws_sdk_s3::types::{Delete, ObjectIdentifier};
 use aws_sdk_s3::Client as S3Client;
-use iceberg::io::FileIO;
+use iceberg::io::{FileIO, FileIOBuilder};
 use iceberg::spec::{TableMetadata, TableMetadataBuilder};
 use iceberg::table::Table;
 use iceberg::Error as IcebergError;
@@ -45,6 +47,7 @@ use iceberg::Result as IcebergResult;
 use iceberg::{
     Catalog, Namespace, NamespaceIdent, TableCommit, TableCreation, TableIdent, TableUpdate,
 };
+use parquet::file::metadata;
 
 const NAMESPACE_INDICATOR_OBJECT_NAME: &str = "indicator.text";
 const PROVIDER: &str = "s3-catalog-provider";
@@ -78,6 +81,7 @@ impl S3CatalogConfig {
 
 #[derive(Debug)]
 pub struct S3Catalog {
+    file_io: FileIO,
     s3_client: S3Client,
     bucket: String,
 }
@@ -85,6 +89,7 @@ pub struct S3Catalog {
 impl S3Catalog {
     pub fn new(config: S3CatalogConfig) -> Self {
         let bucket = config.bucket;
+        let warehouse_location = config.warehouse_location;
         let creds = Credentials::new(
             config.access_key_id,
             config.secret_access_key,
@@ -96,11 +101,12 @@ impl S3Catalog {
             .behavior_version(aws_sdk_s3::config::BehaviorVersion::latest())
             .credentials_provider(creds.clone())
             .region(Region::new(config.region))
-            .endpoint_url(config.warehouse_location)
+            .endpoint_url(warehouse_location.clone())
             .force_path_style(true)
             .build();
         let client = S3Client::from_conf(config);
         Self {
+            file_io: FileIOBuilder::new("s3").build().unwrap(),
             s3_client: client,
             bucket: bucket,
         }
@@ -113,17 +119,16 @@ impl S3Catalog {
             path.push(part);
         }
         path.push(NAMESPACE_INDICATOR_OBJECT_NAME);
-        let object_name = path.to_str().unwrap().to_string();
-        object_name
+        path.to_str().unwrap().to_string()
     }
 
     /// Check whether the given object exists in the bucket.
-    async fn object_exists(&self, key: &str) -> Result<bool, Box<dyn Error>> {
+    async fn object_exists(&self, object: &str) -> Result<bool, Box<dyn Error>> {
         match self
             .s3_client
             .head_object()
             .bucket(self.bucket.clone())
-            .key(key)
+            .key(object)
             .send()
             .await
         {
@@ -197,6 +202,57 @@ impl S3Catalog {
             }
         }
         Ok(())
+    }
+
+    /// List all direct sub-directory under the given directory.
+    ///
+    /// For example, we have directory "a", "a/b", "a/b/c", listing direct subdirectories for "a" will return "a/b".
+    async fn list_direct_subdirectories(
+        &self,
+        folder: &str,
+    ) -> Result<Vec<String>, Box<dyn Error>> {
+        let mut directories = Vec::new();
+        let mut continuation_token = None;
+
+        loop {
+            let mut builder = self
+                .s3_client
+                .list_objects_v2()
+                .bucket(self.bucket.clone())
+                .prefix(folder)
+                .delimiter("/");
+            if let Some(token) = continuation_token {
+                builder = builder.continuation_token(token);
+            }
+            let response = builder.send().await?;
+            if let Some(prefixes) = response.common_prefixes {
+                for prefix in prefixes {
+                    if let Some(prefix_str) = prefix.prefix {
+                        // Remove the trailing "/" and the initial folder/ prefix (if it exists)
+                        let cleaned_prefix = if folder.is_empty() {
+                            prefix_str.trim_end_matches('/').to_string()
+                        } else {
+                            prefix_str
+                                .trim_start_matches(folder)
+                                .trim_end_matches('/')
+                                .to_string()
+                        };
+
+                        // Check if the cleaned prefix itself contains a "/", if it does, it is not a direct subdirectory
+                        if !cleaned_prefix.contains('/') {
+                            directories.push(cleaned_prefix);
+                        }
+                    }
+                }
+            }
+
+            continuation_token = response.next_continuation_token;
+            if continuation_token.is_none() {
+                break;
+            }
+        }
+
+        Ok(directories)
     }
 }
 
@@ -297,8 +353,28 @@ impl Catalog for S3Catalog {
     }
 
     /// List tables from namespace, return error if the given namespace doesn't exist.
-    async fn list_tables(&self, namespace: &NamespaceIdent) -> IcebergResult<Vec<TableIdent>> {
-        todo!()
+    ///
+    /// TODO(hjiang): We should check namespace existence before list.
+    async fn list_tables(
+        &self,
+        namespace_ident: &NamespaceIdent,
+    ) -> IcebergResult<Vec<TableIdent>> {
+        let parent_directory = namespace_ident.to_url_string();
+        let subdirectories = self
+            .list_direct_subdirectories(&parent_directory)
+            .await
+            .map_err(|e| {
+                IcebergError::new(
+                    iceberg::ErrorKind::Unexpected,
+                    format!("Failed to list tables: {}", e),
+                )
+            })?;
+
+        let mut table_idents = Vec::with_capacity(subdirectories.len());
+        for cur_subdir in subdirectories.iter() {
+            table_idents.push(TableIdent::new(namespace_ident.clone(), cur_subdir.clone()));
+        }
+        Ok(table_idents)
     }
 
     async fn update_namespace(
@@ -310,12 +386,66 @@ impl Catalog for S3Catalog {
     }
 
     /// Create a new table inside the namespace.
+    ///
+    /// TODO(hjiang): Confirm the location field inside of `TableCreation`.
     async fn create_table(
         &self,
-        namespace: &NamespaceIdent,
+        namespace_ident: &NamespaceIdent,
         creation: TableCreation,
     ) -> IcebergResult<Table> {
-        todo!()
+        let directory = namespace_ident.to_url_string();
+        let table_ident = TableIdent::new(namespace_ident.clone(), creation.name.clone());
+
+        // Create version hint file.
+        let version_hint_filepath =
+            format!("{}/{}/metadata/version-hint.text", directory, creation.name);
+
+        self.s3_client
+            .put_object()
+            .bucket(self.bucket.clone())
+            .key(version_hint_filepath)
+            .body(ByteStream::from("0".as_bytes().to_vec()))
+            .send()
+            .await
+            .map_err(|e| {
+                IcebergError::new(
+                    iceberg::ErrorKind::Unexpected,
+                    format!(
+                        "Failed to create version hint file on table creation: {}",
+                        e
+                    ),
+                )
+            })?;
+
+        // Create metadata file.
+        let metadata_filepath = format!(
+            "{}/{}/metadata/v0.metadata.json",
+            directory,
+            creation.name.clone()
+        );
+
+        let table_metadata = TableMetadataBuilder::from_table_creation(creation)?.build()?;
+        let metadata_json = serde_json::to_string(&table_metadata.metadata)?;
+        self.s3_client
+            .put_object()
+            .bucket(self.bucket.clone())
+            .key(metadata_filepath)
+            .body(ByteStream::from(metadata_json.as_bytes().to_vec()))
+            .send()
+            .await
+            .map_err(|e| {
+                IcebergError::new(
+                    iceberg::ErrorKind::Unexpected,
+                    format!("Failed to create metadata file on table creation: {}", e),
+                )
+            })?;
+
+        let table = Table::builder()
+            .metadata(table_metadata.metadata)
+            .identifier(table_ident)
+            .file_io(self.file_io.clone())
+            .build()?;
+        Ok(table)
     }
 
     /// Load table from the catalog.
@@ -330,11 +460,25 @@ impl Catalog for S3Catalog {
 
     /// Check if a table exists in the catalog.
     async fn table_exists(&self, table: &TableIdent) -> IcebergResult<bool> {
-        todo!()
+        let mut version_hint_filepath = PathBuf::from(table.namespace.to_url_string());
+        version_hint_filepath.push(table.name());
+        version_hint_filepath.push("metadata");
+        version_hint_filepath.push("version-hint.text");
+
+        let exists = self
+            .object_exists(&version_hint_filepath.to_str().unwrap().to_owned())
+            .await
+            .map_err(|e| {
+                IcebergError::new(
+                    iceberg::ErrorKind::Unexpected,
+                    format!("Failed to check object existence: {}", e),
+                )
+            })?;
+        Ok(exists)
     }
 
     /// Rename a table in the catalog.
-    async fn rename_table(&self, src: &TableIdent, dest: &TableIdent) -> IcebergResult<()> {
+    async fn rename_table(&self, _src: &TableIdent, _dest: &TableIdent) -> IcebergResult<()> {
         todo!()
     }
 
@@ -379,6 +523,42 @@ mod tests {
         s3_catalog
     }
 
+    // Test util function to get iceberg schema,
+    async fn get_test_schema() -> IcebergResult<Schema> {
+        let field = NestedField::required(
+            /*id=*/ 1,
+            "field_name".to_string(),
+            IcebergType::Primitive(PrimitiveType::Int),
+        );
+        let schema = Schema::builder()
+            .with_schema_id(0)
+            .with_fields(vec![Arc::new(field)])
+            .build()?;
+
+        Ok(schema)
+    }
+
+    // Test util function to create a new table.
+    async fn create_test_table(catalog: &S3Catalog) -> IcebergResult<()> {
+        // Define namespace and table.
+        let namespace = NamespaceIdent::from_strs(["default"])?;
+        let table_name = "test_table".to_string();
+
+        let schema = get_test_schema().await?;
+        let table_creation = TableCreation::builder()
+            .name(table_name.clone())
+            .location(format!("s3://{}/{}", namespace.to_url_string(), table_name))
+            .schema(schema.clone())
+            .build();
+
+        catalog
+            .create_namespace(&namespace, /*properties=*/ HashMap::new())
+            .await?;
+        catalog.create_table(&namespace, table_creation).await?;
+
+        Ok(())
+    }
+
     #[tokio::test]
     async fn test_s3_catalog_namespace_operations() -> IcebergResult<()> {
         let catalog = create_s3_catalog().await;
@@ -412,6 +592,31 @@ mod tests {
         catalog.drop_namespace(&namespace).await?;
         let exists = catalog.namespace_exists(&namespace).await?;
         assert!(!exists, "Namespace should not exist after drop");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_s3_catalog_table_operations() -> IcebergResult<()> {
+        let catalog = create_s3_catalog().await;
+
+        // Define namespace and table.
+        let namespace = NamespaceIdent::from_vec(vec!["default".to_string()])?;
+        let table_name = "test_table".to_string();
+        let table_ident = TableIdent::new(namespace.clone(), table_name.clone());
+
+        // Ensure table does not exist.
+        let table_already_exists = catalog.table_exists(&table_ident).await?;
+        assert!(
+            !table_already_exists,
+            "Table should not exist before creation"
+        );
+
+        // TODO(hjiang): Add testcase to check list table here.
+
+        create_test_table(&catalog).await?;
+        let table_already_exists = catalog.table_exists(&table_ident).await?;
+        assert!(table_already_exists, "Table should exist after creation");
 
         Ok(())
     }
